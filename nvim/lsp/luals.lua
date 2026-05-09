c08ad42ffb2d76f56d4b574177313594dd7b357a
Jesus Saez <yisussaez@gmail.com>
2026-05-09 01:47:06 -0300

    Better neovim completion and added nvim workspace to lua lsp

--- nvim/lsp/luals.lua
+++ nvim/lsp/luals.lua
@@ -20,6 +20,11 @@ return {
       diagnostics = {
         globals = { 'vim' }
       },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false, -- Optional: disable prompt for third-party libs
+      },
       runtime = {
         version = 'LuaJIT',
       }
